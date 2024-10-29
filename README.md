486857a5e40993f6b8b0c8b0224f7d925ec3e1e6
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2024-10-29 19:44:25 +0800

    chore: release 1.0.4

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-https://registry.npmmirror.com/@jiakun-zhao/css/latest/files/dist/index.css
+# @jiakun-zhao/css
+
+```ts
+import { name, version } from '@jiakun-zhao/css'
+
+const href = `https://registry.npmmirror.com/${name}/${version}/files/dist/index.css`
+```
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,4 @@ import { name, version } from '../package.json'
 
 import '~/assets/index.css'
 
-const link = document.createElement('link')
-
-link.rel = 'stylesheet'
-link.crossOrigin = ''
-link.href = `https://registry.npmmirror.com/${name}/${version}/files/dist/index.css`
-
-document.head.appendChild(link)
+export const href = `https://registry.npmmirror.com/${name}/${version}/files/dist/index.css`
